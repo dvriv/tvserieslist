1463004f566e223993608b18223a3190cfcae8fe
David <djro1605@gmail.com>
2018-03-12 01:45:06 -0600

    db config file

---
+++ db/db.js
@@ -0,0 +1,15 @@
+// I have to execute 'source env.sh'
+
+const { Pool, Client } = require('pg');
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDBTVSERIESLIST,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
+
+module.exports = {
+  query: (text, params, callback) => pool.query(text, params, callback),
+};
