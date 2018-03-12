1b0dc651c1fc8165c49838aa46f492d45d301d41
David <djro1605@gmail.com>
2018-03-12 01:35:55 -0600

    added body parser, db require, db file and routes to user.route.js

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 8080;
+const db = require('./db/db');
+const bodyParser = require('body-parser');
+
+const userRouter = require('./api/user/user.route.js');
+
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //  Public folder and Pug
 app.use(express.static('site/public'));
@@ -13,8 +21,9 @@ app.get('/', (req, res) => res.render('homepage'));
 app.get('/login', (req, res) => res.render('login'));
 app.get('/signup', (req, res) => res.render('signup'));
 
+// API routes are on every API folder
 
-// Api Renders
+app.use('/api/users', userRouter);
 
 
 const server = app.listen(port, () => console.log(`Listening on port ${server.address().port}`));
